6023dfdd6cf0f940e4ed19d0e029e68b041572ff
Nazmus Sakib <nazmuss247@gmail.com>
2025-04-16 00:04:39 +0600

    Uploading Basic

---
+++ 00_Basic_Intro/03_Comment.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std;
+int main() {
+    cout << "Hellow Good people\n";
+    // This is a single line comment
+    cout << "I'm Learning C++ language";
+    /* This is a multiline comment.
+    I,m a newbie in c++*/
+    return 0;
+}
