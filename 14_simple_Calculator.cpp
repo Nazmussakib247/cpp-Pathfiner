7e97240b9efe1c437621696ecc43a26f2757f818
Nazmus Sakib <nazmuss247@gmail.com>
2025-10-29 23:13:31 +0600

    added

---
+++ 14_simple_Calculator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x, y, sum;
+    cout << "Enter first Number: " ;
+    cin >> x;
+    cout << "Enter second Number: ";
+    cin >> y;
+    sum = x + y;
+    cout << "Your total sum is: " << sum;
+    return 0 ;
+}
