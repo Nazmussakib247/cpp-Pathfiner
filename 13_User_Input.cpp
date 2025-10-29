230b59e4cc1390db6ff78e5097454ccf9c8c4fb0
Nazmus Sakib <nazmuss247@gmail.com>
2025-10-29 23:13:22 +0600

    added

---
+++ 13_User_Input.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cout << "Type a number: "; //type number
+    cin >> x; //taking user input
+    cout << "Your number is : " << x;
+}
